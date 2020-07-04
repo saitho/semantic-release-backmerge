62b0e5778c578864cad57d52a0dde6e430e7bb8e
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-04 14:04:57 +0200

    test: run without plugins configuration

--- test/perform-backmerge.test.ts
+++ test/perform-backmerge.test.ts
@@ -33,6 +33,31 @@ describe("perform-backmerge", () => {
             .catch((error) => done(error));
     });
 
+    it("works without plugin definition", (done) => {
+        const mockedGit = mock(Git);
+        const mockedLogger = mock(NullLogger);
+        when(mockedGit.checkout(anyString())).thenResolve();
+        when(mockedGit.configFetchAllRemotes()).thenResolve();
+        when(mockedGit.getStagedFiles()).thenResolve([]);
+        when(mockedGit.fetch()).thenResolve();
+        when(mockedGit.rebase(anyString())).thenResolve();
+        when(mockedGit.push(anyString(), anyString(), anything())).thenResolve();
+
+        const context = {logger: instance(mockedLogger), branch: {name: 'master'}, options: {repositoryUrl: 'my-repo'}};
+        performBackmerge(instance(mockedGit), {branchName: 'develop'}, context)
+            .then(() => {
+                verify(mockedLogger.log('Release succeeded. Performing back-merge into branch "develop".')).once();
+                verify(mockedGit.checkout('master')).once();
+                verify(mockedGit.configFetchAllRemotes()).once();
+                verify(mockedGit.fetch()).once();
+                verify(mockedGit.checkout('develop')).once();
+                verify(mockedGit.rebase('master')).once();
+                verify(mockedGit.push('my-repo', 'develop', false)).once();
+                done();
+            })
+            .catch((error) => done(error));
+    });
+
     it("with force-push", (done) => {
         const mockedGit = mock(Git);
         const mockedLogger = mock(NullLogger);

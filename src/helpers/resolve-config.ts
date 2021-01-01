138abb52e1f68e8fb98cfa7a96da9348a1eb0fa0
Mario Lubenka <mario.lubenka@googlemail.com>
2021-01-01 17:15:55 +0100

    fix: add setting allowSameBranchMerge (#6)
    To perform backmerges when source and target branch are the same,
    the setting "allowSameBranchMerge" needs to be enabled.
    
    This setting is disabled per default.

--- src/helpers/resolve-config.ts
+++ src/helpers/resolve-config.ts
@@ -1,10 +1,11 @@
 import {isNil} from 'lodash';
 
-export function resolveConfig({branchName, message, plugins, forcePush}) {
+export function resolveConfig({branchName, message, plugins, forcePush, allowSameBranchMerge}) {
     return {
         branchName: isNil(branchName) ? 'develop' : branchName,
         plugins: isNil(plugins) ? [] : plugins,
         message: message,
         forcePush: isNil(forcePush) ? false : forcePush,
+        allowSameBranchMerge: isNil(allowSameBranchMerge) ? false : allowSameBranchMerge
     };
 }

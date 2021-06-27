c3ab08776bcd3ea5a61949ab00a74aaf7aa71e34
Mario Lubenka <mario.lubenka@googlemail.com>
2021-06-27 19:43:20 +0200

    chore(test): resolve Jest deprecation
    Rename deprecated "tsConfig" setting to "tsconfig"

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     testEnvironment: 'node',
     globals: {
         'ts-jest': {
-            tsConfig: 'tsconfig.json',
+            tsconfig: 'tsconfig.json',
         },
     },
     moduleFileExtensions: [

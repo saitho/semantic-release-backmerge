7ca2ba4511b40ede179bb26c0d7c492fc611a131
Semaph0r <semaph0r@posteo.de>
2022-02-02 11:17:26 +0100

    test: test error logging for unstaged changes during rebase

--- test/perform-backmerge.test.ts
+++ test/perform-backmerge.test.ts
@@ -519,6 +519,71 @@ describe("perform-backmerge to multiple branches", () => {
     });
 });
 
+describe("perform-backmerge with error", () => {
+    jest.mock('semantic-release/lib/get-git-auth-url', () => jest.fn((c) => c.options.repositoryUrl));
+    it("rebase with unstaged changes", (done) => {
+        class MockError extends Error {
+            stderr: string
+        }
+        const mockedGit = mock(Git);
+        const mockedLogger = mock(NullLogger);
+        when(mockedGit.checkout(anyString())).thenResolve();
+        when(mockedGit.configFetchAllRemotes()).thenResolve();
+        when(mockedGit.fetch()).thenResolve();
+        when(mockedGit.rebase(anyString())).thenThrow({message:'',name:'',stderr:'have unstaged changes'} as MockError);
+        when(mockedGit.getModifiedFiles()).thenReturn(new Promise<string[]>(resolve => resolve(['M testfile'])));
+        when(mockedLogger.error(anyString())).thenResolve();
+
+        const context = {logger: instance(mockedLogger), branch: {name: 'master'}, options: {repositoryUrl: 'my-repo'}};
+        performBackmerge(instance(mockedGit), {branches: ['develop']}, context)
+            .then(() => {
+                verify(mockedLogger.log('Performing back-merge into branch "develop".')).once();
+                verify(mockedLogger.error('Invalid branch configuration found and ignored.')).never();
+                verify(mockedGit.checkout('master')).once();
+                verify(mockedGit.configFetchAllRemotes()).once();
+                verify(mockedGit.fetch(context.options.repositoryUrl)).once();
+                verify(mockedGit.checkout('develop')).once();
+                verify(mockedGit.rebase('master')).once();
+                verify(mockedGit.getModifiedFiles()).once();
+                verify(mockedLogger.error('1 modified file(s):')).once();
+                verify(mockedLogger.error('M testfile')).once();
+                verify(mockedGit.push('my-repo', 'develop', false)).never();
+                done();
+            })
+            .catch((error) => done(error));
+    });
+
+    it("rebase with other error", (done) => {
+        class MockError extends Error {
+            stderr: string
+        }
+        const mockedGit = mock(Git);
+        const mockedLogger = mock(NullLogger);
+        when(mockedGit.checkout(anyString())).thenResolve();
+        when(mockedGit.configFetchAllRemotes()).thenResolve();
+        when(mockedGit.getModifiedFiles()).thenResolve([]);
+        when(mockedGit.fetch()).thenResolve();
+        when(mockedGit.rebase(anyString())).thenThrow({message:'',name:'',stderr:'any other error'} as MockError);
+        when(mockedGit.getModifiedFiles()).thenResolve();
+
+        const context = {logger: instance(mockedLogger), branch: {name: 'master'}, options: {repositoryUrl: 'my-repo'}};
+        performBackmerge(instance(mockedGit), {branches: ['develop']}, context)
+            .then(() => {
+                verify(mockedLogger.log('Performing back-merge into branch "develop".')).once();
+                verify(mockedLogger.error('Invalid branch configuration found and ignored.')).never();
+                verify(mockedGit.checkout('master')).once();
+                verify(mockedGit.configFetchAllRemotes()).once();
+                verify(mockedGit.fetch(context.options.repositoryUrl)).once();
+                verify(mockedGit.checkout('develop')).once();
+                verify(mockedGit.rebase('master')).once();
+                verify(mockedGit.getModifiedFiles()).never();
+                verify(mockedGit.push('my-repo', 'develop', false)).never();
+                done();
+            })
+            .catch((error) => done(error));
+    });
+});
+
 // todo: remove with next major release when `branchName` is removed
 describe("perform-backmerge with deprecated branchName setting", () => {
     jest.mock('semantic-release/lib/get-git-auth-url', () => jest.fn((c) => c.options.repositoryUrl));
